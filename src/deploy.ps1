f0e69e65b643d5723f9446310e54a3943dd2e3ce
Johan Olsson <johan.olsson@cab.se>
2024-10-09 21:16:52 +0200

    path fix

--- src/deploy.ps1
+++ src/deploy.ps1
@@ -3,4 +3,4 @@ param(
 )
 
 # Azure Infra Deploy
-az stack sub create --name ChangeCapture --location swedencentral --template-file ".\infrastructure\main.bicep" --parameters ".\infrastructure\main.bicepparam" sqlServerPassword=$sqlServerDbPassword --dm none --yes | ConvertFrom-Json
+az stack sub create --name ChangeCapture --location swedencentral --template-file ".\iac\main.bicep" --parameters ".\iac\main.bicepparam" sqlServerPassword=$sqlServerDbPassword --dm none --yes | ConvertFrom-Json
